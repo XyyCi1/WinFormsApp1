a93c5893c1d9cbce999bce5d9d65704d04ad43b1
Franxyz zyann Olaño <hed-fzolano@smu.edu.ph>
2025-12-01 18:17:02 +0800

    logo

---
+++ WinFormsApp1/HomeMenu.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WinFormsApp1
+{
+    partial class HomeMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeMenu));
+            label1 = new Label();
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(200, 47);
+            label1.TabIndex = 0;
+            label1.Text = "USERNAME";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(252, 326);
+            button1.Name = "button1";
+            button1.Size = new Size(267, 49);
+            button1.TabIndex = 1;
+            button1.Text = "View Match History";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(292, 392);
+            button3.Name = "button3";
+            button3.Size = new Size(196, 49);
+            button3.TabIndex = 3;
+            button3.Text = "LeaderBoard";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(228, 257);
+            button4.Name = "button4";
+            button4.Size = new Size(316, 49);
+            button4.TabIndex = 4;
+            button4.Text = "PLAY";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaption;
+            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(800, 101);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.ActiveCaption;
+            pictureBox2.Dock = DockStyle.Bottom;
+            pictureBox2.Location = new Point(0, 466);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(800, 94);
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = SystemColors.ActiveCaption;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(637, 5);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(120, 96);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 10;
+            pictureBox6.TabStop = false;
+            // 
+            // HomeMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 560);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "HomeMenu";
+            Text = "UserProfile";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox6;
+    }
+}
