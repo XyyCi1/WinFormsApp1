4aceeba9f93421d91dd862ace43f871ad3e16911
Franxyz zyann Olaño <hed-fzolano@smu.edu.ph>
2025-12-01 17:52:38 +0800

    Added leaderboard ui (not final)

---
+++ WinFormsApp1/Leaderboard.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WinFormsApp1
+{
+    partial class Leaderboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Gill Sans Ultra Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(141, 76);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(306, 31);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "LEADERBOARD";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaption;
+            pictureBox1.Location = new Point(85, 43);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(436, 458);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(106, 57);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(397, 409);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(117, 113);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(373, 337);
+            dataGridView1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(106, 472);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "BACK";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Leaderboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 524);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Leaderboard";
+            Text = "Leaderboard";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private DataGridView dataGridView1;
+        private Button button1;
+    }
+}
